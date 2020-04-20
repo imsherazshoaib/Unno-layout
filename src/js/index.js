4b4be4679f238bd2d0469a53807dfc20d823457d
msherazshoaib <sherazshoaib123@gmail.com>
2020-04-20 16:29:23 +0500

    work in progress

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,9 @@
 import '@fortawesome/fontawesome-free/js/all.js'
+function show_hide() {
+  var click = documment.getElementById('drop-content')
+  if (click.style.display === 'none') {
+    click.style.display = 'block'
+  } else {
+    click.style.display = 'none'
+  }
+}
